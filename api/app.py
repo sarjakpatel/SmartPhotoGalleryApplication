f753b1e1224c4d0a336c125b8ed1634bbdb70256
Vishnu Vardhan Reddy yeruva <vishnuvardhan.reddy98@gmail.com>
2023-04-15 04:33:03 -0600

    commit api

--- api/app.py
+++ api/app.py
@@ -1,37 +1,48 @@
 #reference: https://medium.com/makedeveasy/authenitcation-using-python-flask-and-firestore-1958d29e2240
 
-
-#import libraries
 import os
-import io
-import json
-from functools import wraps
-
-from flask import Flask, request, jsonify, redirect, session, send_file
-from firebase_admin import credentials, firestore, initialize_app, auth
+from flask import Flask, request, jsonify
+from firebase_admin import credentials, firestore, initialize_app
 import firebase_admin
 import pyrebase
+<<<<<<< Updated upstream
 from flask_cors import CORS
 import io
 import json
 from firebase_admin import auth
-
-#auth = auth()
-
-CORS(app)
-
-db_connect = firestore.client()
-todo_ref = db_connect.collection('todos')  #sample collections ##############
-
-firebase = pyrebase.initialize_app(json.load(open('fbconfig.json')))
-
-<<<<<<< Updated upstream
 =======
+from werkzeug.utils import secure_filename
+import os  
+from flask import Flask, render_template, request
+from ocr_core import ocr_core
+
+#from face_encodings import search_similar_image, store_encodings, check_face_encodings
+from data import check_encodings, search_similar_image
+
+import cv2
+import numpy
+
+app = Flask(__name__)
+
+firebase_admin.delete_app(firebase_admin.get_app())
+
+cred = credentials.Certificate('/home/vishnu-yeruva/Documents/Edu/CMPE 295-B/Project/api/fbAdminConfig.json')
+default_app = firebase_admin.initialize_app(cred)
+>>>>>>> Stashed changes
+
+
+app=Flask(__name__)
+cred = credentials.Certificate("/home/vishnu-yeruva/Documents/Edu/CMPE295B/Project/SmartPhotoGalleryApplication/api/fbAdminConfig.json")
+default_app=firebase_admin.initialize_app(cred)
+CORS(app)
+db = firestore.client()
+todo_ref=db.collection('todos')  #sample collections
+pb = pyrebase.initialize_app(json.load(open('/home/vishnu-yeruva/Documents/Edu/CMPE295B/Project/SmartPhotoGalleryApplication/api/fbconfig.json')))
+
 db_connect = firestore.client()
 todo_ref = db_connect.collection('todos')  #sample collections ##############
 
 firebase = pyrebase.initialize_app(json.load(open('/home/vishnu-yeruva/Documents/Edu/CMPE 295-B/Project/api/fbconfig.json')))
->>>>>>> Stashed changes
 
 #signup api
 
@@ -68,9 +79,37 @@ def signup():
                 return jsonify({'message': 'user already exists '}), 400
     
         else:
-            return jsonify({'message': 'error creating in user'}), 401
+            return jsonify({'message':'please verify your account with your mailId'}),401
+    except:
+        return jsonify({'message':'invalid crendentails or user does not exist'}),403
+<<<<<<< Updated upstream
+=======
+
+'''
+#logout api
+
+@app.route("/logout")
+
+def logout():
+    #remove the token setting the user to None
+    session.pop('username')
+    return redirect("/login")
+
+'''
 
 
+#to protect routes
+
+def isAuthenticated(f):
+  @wraps(f)
+  def decorated_function(*args, **kwargs):
+      #check for the variable that pyrebase creates
+      if not firebase.auth() != None:
+          return redirect('/login')
+      return f(*args, **kwargs)
+  return decorated_function
+    
+  
 
 
 #login api
@@ -108,9 +147,60 @@ def login():
             return jsonify({'message': 'please verify your account with your mailId'}), 401
     
     
-    #invalid credentials
-    except:
-        return jsonify({'message':'invalid crendentails or user does not exist'}),403
+    elif img_matrix is None:
+        return jsonify({'message': 'upload file'}), 400
+
+    elif email and img_matrix:
+        output_urls, keys, match1 = search_similar_image(email, img_matrix)
+        
+
+        return jsonify({'list of similar images': output_urls}), 200
+    
+
+###########################################################################
+
+UPLOAD_FOLDER = '/static/uploads/'
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+app = Flask(__name__)
+
+def allowed_file(filename):  
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def home_page():  
+    return render_template('index.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_page():  
+    if request.method == 'POST':
+        # check if there is a file in the request
+        if 'file' not in request.files:
+            return render_template('upload.html', msg='No file selected')
+        file = request.files['file']
+        # if no file is selected
+        if file.filename == '':
+            return render_template('upload.html', msg='No file selected')
+
+        if file and allowed_file(file.filename):
+
+            # call the OCR function on it
+            extracted_text = ocr_core(file)
+
+            # extract the text and display it
+            return render_template('upload.html',
+                                   msg='Successfully processed',
+                                   extracted_text=extracted_text,
+                                   img_src=UPLOAD_FOLDER + file.filename)
+    elif request.method == 'GET':
+        return render_template('upload.html')
+
+##########################################################################
+
+
+>>>>>>> Stashed changes
 if __name__ == '__main__':
 
     app.run(debug = True)
